3b66bffab5e4b7ba5182684062885b800fe711e0
Zaid J. Barghouthi <barghouthi.z@gmail.com>
2024-06-19 19:03:20 +0300

    file reader app

---
+++ week_2/FileReaderApplication/src/main.rs
@@ -0,0 +1,36 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        panic!("File name missing!");
+    }
+
+    let file = File::open(args[1].as_str());
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => {
+            match error.kind() {
+                std::io::ErrorKind::NotFound => {
+                    panic!("File not found: {}", error)
+                }
+                _ => {
+                    panic!("Error opening file: {}", error)
+                }
+            }
+        }
+    };
+    
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        match line {
+            Ok(line) => println!("{}", line),
+            Err(error) => {
+                panic!("Error reading line: {}", error)
+            }
+        }
+    }
+}
